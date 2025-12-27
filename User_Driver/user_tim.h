485037c8e6a5d8cd48fede5d3967932aadc1b612
Zach <zach20722303@outlook.com>
2025-12-28 01:32:00 +0800

    fix: unify TIMx_CLKO_PIN and BUZZx

--- User_Driver/user_tim.h
+++ User_Driver/user_tim.h
@@ -7,9 +7,9 @@
 #define TIM3                3
 #define TIM4                4
 
-#define TIM1_CLKO_PIN       P34
-#define TIM3_CLKO_PIN       P04
-#define TIM4_CLKO_PIN       P06
+#define TIM1_CLKO_PIN       BUZZ1
+#define TIM3_CLKO_PIN       BUZZ2
+#define TIM4_CLKO_PIN       BUZZ3
 
 void User_TIM_SetARR(u8 TIMx, u16 arr);
 void User_TIM_CLKOCmd(u8 TIMx, u8 cmd);
